fa79ab1e7033807196df792f21bcf58d7ec7317d
robertrowe1013 <robertrowe1013@gmail.com>
2020-02-06 02:32:32 +0000

    another try at 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
 /**
-  * puts - print string
+  * main - print string
+  * Return: return 0
  */
-int main (void)
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	
-	return 0;
+	return (0);
 }
