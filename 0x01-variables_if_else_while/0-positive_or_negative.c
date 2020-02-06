86d48629f7ab94e761a4f7a73c8588d0730544b0
robertrowe1013 <robertrowe1013@gmail.com>
2020-02-06 19:46:33 +0000

    task 0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,4 +1,19 @@
 #include<stdio.h>
 /**
 * main - identify a number as positive, negative, or zero.
+*
+* Return: return 0
 */
+int main(void)
+{
+	int n;
+
+	if (n > 0)
+		printf("%d is positive\n", n);
+	if (n < 0)
+		printf("%d is negative\n", n);
+	if (n == 0)
+		printf("%d is zero\n", n);
+
+	return (0);
+}
