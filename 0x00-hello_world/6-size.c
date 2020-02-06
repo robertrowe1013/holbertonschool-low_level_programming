f4a67455a621b562bb3fb6b8af5195a917000c74
robertrowe1013 <robertrowe1013@gmail.com>
2020-02-06 03:56:22 +0000

    task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,2 +1,22 @@
-#!/bin/bash
+#include<stdio.h>
+/**
+  * main - print byte usage
+  *
+  *Return: return 0
+  */
+int main(void)
+{
+	char character;
+	int integer;
+	long longinteger;
+	long long verylonginteger;
+	float floatingdecimal;
 
+	printf("Size of a char: %lu byte(s)\n", sizeof(character));
+	printf("Size of an int: %lu byte(s)\n", sizeof(integer));
+	printf("Size of a long int: %lu byte(s)\n", sizeof(longinteger));
+	printf("Size of a long long int: %lu byte(s)\n", sizeof(verylonginteger));
+	printf("Size of a float: %lu byte(s)\n", sizeof(floatingdecimal));
+
+	return (0);
+}
