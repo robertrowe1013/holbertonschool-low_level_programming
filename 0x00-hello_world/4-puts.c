fab7e9905b306160b7e2f8e609f7d6e2c75b85fa
robertrowe1013 <robertrowe1013@gmail.com>
2020-02-06 02:56:52 +0000

    include header for 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
+#include<stdio.h>
 /**
   * main - print string
   * Return: return 0
  */
 int main(void)
 {
-	char str1[5] = "test";
+	char str1[] = "\"Programming is like building a multilingual puzzle";
 
 	puts(str1);
 	return (0);
