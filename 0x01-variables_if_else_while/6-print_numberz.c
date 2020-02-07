a469e589a36ed4f1022a0761429291e3a6fbb3a8
robertrowe1013 <robertrowe1013@gmail.com>
2020-02-07 01:49:57 +0000

    task 6

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,2 +1,20 @@
 #include<stdio.h>
+/**
+ * main - print numbers without char variable
+ *
+ * Return: returns 0
+ */
+int main(void)
+{
+	int n = 0;
+
+	while (n < 10)
+	{
+		putchar('0' + n);
+		n++;
+	}
+	putchar ('\n');
+
+	return (0);
+}
 
