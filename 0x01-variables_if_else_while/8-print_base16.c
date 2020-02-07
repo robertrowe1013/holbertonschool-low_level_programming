a782d01a78e4def1462cd7a1bac35f9e0c37f22a
robertrowe1013 <robertrowe1013@gmail.com>
2020-02-07 02:09:26 +0000

    task 8

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,1 +1,25 @@
 #include<stdio.h>
+/**
+ * main - display lowercase alphabet without printf
+ *
+ * Return: return 0
+ */
+int main(void)
+{
+	char deci = '0';
+	char hexi = 'a';
+
+	while (deci <= '9')
+	{
+		putchar(deci);
+		deci++;
+	}
+	while (hexi <= 'f')
+	{
+		putchar(hexi);
+		hexi++;
+	}
+	putchar('\n');
+
+	return (0);
+}
