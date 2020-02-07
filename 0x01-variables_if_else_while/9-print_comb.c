de4f494010bd2675f291b91b83349705f9b938b1
robertrowe1013 <robertrowe1013@gmail.com>
2020-02-07 02:52:37 +0000

    task 9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,2 +1,24 @@
 #include<stdio.h>
+/**
+  * main - print numbers seperated by commas
+  *
+  * Return: 0
+  */
+int main(void)
+{
+	int i = 48;
 
+	while (i <= 57)
+	{
+		putchar(i);
+		if (i != 57)
+		{
+			putchar(44);
+			putchar(32);
+		}
+		i++;
+	}
+	putchar('\n');
+
+	return (0);
+}
