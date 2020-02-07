00766547fc722f3920f43b851c86c1c755f30348
robertrowe1013 <robertrowe1013@gmail.com>
2020-02-07 00:45:36 +0000

    task 4

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,2 +1,22 @@
 #include<stdio.h>
+/**
+ * main - display lowercase alphabet without e or q
+ *
+ * Return: return 0
+ */
+int main(void)
+{
+	char letter = 'a';
 
+	while (letter <= 'z')
+	{
+		if (letter == 'e' || 'q')
+			letter++;
+		else
+		putchar(letter);
+		letter++;
+	}
+	putchar('\n');
+
+	return (0);
+}
