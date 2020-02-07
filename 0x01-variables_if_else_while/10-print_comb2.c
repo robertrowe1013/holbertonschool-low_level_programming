e9eb2fab0a8441247850fdceeef97fde089c77a0
robertrowe1013 <robertrowe1013@gmail.com>
2020-02-07 04:33:08 +0000

    task 10

--- 0x01-variables_if_else_while/10-print_comb2.c
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -1,1 +1,36 @@
 #include<stdio.h>
+/**
+  * main - iterate numbers 00 through 99
+  *
+  * Return: 0
+  */
+int main(void)
+{
+	int tens = 48;
+	int ones = 48;
+
+	while (tens <= 57 && ones <= 57)
+	{
+		putchar (tens);
+		putchar (ones);
+		if (tens == 57 && ones == 57)
+			;
+		else
+		{
+			putchar(44);
+			putchar(32);
+		}
+		if (ones == 57)
+		{
+			tens++;
+			ones = 48;
+		}
+		else
+		{
+			ones++;
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
