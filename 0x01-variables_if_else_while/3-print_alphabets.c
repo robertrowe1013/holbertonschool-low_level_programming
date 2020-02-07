8b1e63e55cfdd5e3db1c9f515cc466f21e7603f6
robertrowe1013 <robertrowe1013@gmail.com>
2020-02-07 00:07:14 +0000

    task 3

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,2 +1,26 @@
 #include<stdio.h>
+/**
+ * main - display lowercase alphabet without printf
+ *
+ * Return: return 0
+ */
+int main(void)
+{
+	char letter = 'a';
+	char cap = 'A';
 
+	while (letter <= 'z')
+	{
+		putchar(letter);
+		letter++;
+	}
+	while (cap <= 'Z')
+	{
+		putchar(cap);
+		cap++;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
