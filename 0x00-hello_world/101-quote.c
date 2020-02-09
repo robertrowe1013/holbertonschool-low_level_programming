c59d8ad04cf1d5029f3369e122cb5a931f98161c
robertrowe1013 <robertrowe1013@gmail.com>
2020-02-09 00:34:28 +0000

    task 101

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,15 +2,15 @@
 #include <unistd.h>
 #include <string.h>
 /**
-* main - print quote
-*
-* Return: 1
-*/
+ * main - print quote
+ *
+ * Return: 1
+ */
 int main(void)
 {
 	char q[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
-	write(2, q, strlen(q));
+	write(2, q, 59);
 
 	return (1);
 }
