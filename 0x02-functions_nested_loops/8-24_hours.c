92bce046c2c7489363b7b0ae839b0ed9f0a6f883
robertrowe1013 <robertrowe1013@gmail.com>
2020-02-11 20:07:58 +0000

    task 8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -4,46 +4,49 @@
   */
 void jack_bauer(void)
 {
-	int hr10 = '0';
-	int hr1 = '0';
-	int min10 = '0';
-	int min1 = '0';
+	int hr10 = 0;
+	int hr1 = 0;
+	int min10 = 0;
+	int min1 = 0;
+	int linecount = 0;
 
-	while (hr10 < '2' && hr1 < '4' && min10 < '0' && min1 < '0')
+	while (linecount < 1440)
 	{
-		_putchar(hr10);
-		_putchar(hr1);
+		_putchar(hr10 + '0');
+		_putchar(hr1 + '0');
 		_putchar(':');
-		_putchar(min10);
-		_putchar(min1);
+		_putchar(min10 + '0');
+		_putchar(min1 + '0');
 		_putchar('\n');
-		
-		if (min1 < '9')
+		linecount++;
+
+		if (min1 < 9)
 		{
 			min1++;
 		}
 		else
 		{
 			min10++;
-			min1 = '0';
-			if (min10 < '5')
+			min1 = 0;
+			if (min10 < 6)
 			{
-				min10++;
+				;
 			}
 			else
 			{
 				hr1++;
-				min10 = '0';
-				if (hr1 < '9')
+				min10 = 0;
+				if (hr1 < 10)
 				{
-					hr1++;
+					;
 				}
 				else
 				{
 					hr10++;
-					hr1 = '0';
+					hr1 = 0;
 				}
 			}
 		}
 	}
+	_putchar('\n');
 }
