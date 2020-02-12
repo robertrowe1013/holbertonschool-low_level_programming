f861b054c7381fa562e7ae68ae22e9730091fde7
robertrowe1013 <robertrowe1013@gmail.com>
2020-02-12 21:39:49 +0000

    task 12

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -6,32 +6,29 @@
   */
 int main(void)
 {
-	int ones = '2';
-	int tens = '1';
-	int hunds = '0';
+	int hund = -1;
+	int ten = 0;
+	int one = 1;
 
-	while (hunds <= '7' && tens <= '8' && ones <= '9')
+	while (hund < 7)
 	{
-		putchar (hunds);
-		putchar (tens);
-		putchar (ones);
-		if (hunds == '7' && tens == '8' && ones == '9')
-			;
-		else
+		hund++;
+		while (ten < 8)
 		{
-			putchar(',');
-			putchar(' ');
+			ten++;
+			while (one < 9)
+			{
+				one++;
+				putchar(hund + '0');
+				putchar(ten + '0');
+				putchar(one +'0');
+				putchar(',');
+				putchar(' ');
+			}
+			one = ten + 1;
 		}
-		if (ones == '9')
-		{
-			hunds++;
-			tens = (hunds + 1);
-			ones = (tens + 1);
-		}
-		else
-			ones++;
+		ten = hund;
 	}
 	putchar('\n');
-
 	return (0);
 }
