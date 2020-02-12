cd5cc94805ba0862b31b27da8056d169d25f2b13
robertrowe1013 <robertrowe1013@gmail.com>
2020-02-12 03:19:48 +0000

    task

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -8,21 +8,23 @@ void print_to_98(int n)
 {
 	if (n < 98)
 	{
-		for (n = n; n == 98; n++)
+		while (n < 98)
 		{
 			printf("%d, ", n);
+			n++;
 		}
 	}
 	else if (n > 98)
 	{
-		for (n = n; n == 98; n--)
+		while (n > 98)
 		{
 			printf("%d, ", n);
+			n--;
 		}
 	}
 	else
 	{
 		;
 	}
-	printf("%d, ", n);
-
+	printf("%d\n", n);
+}
