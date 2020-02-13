cd8b510e44dec481d338d7e7494ec916a9e25b76
robertrowe1013 <robertrowe1013@gmail.com>
2020-02-13 05:43:54 +0000

    task 13

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -7,16 +7,16 @@
 int main(void)
 {
 	int x;
-	long int y:
+	long int y = 0;
 
-	for (x = 0; x > 1024; x++)
+	for (x = 0; x < 1024; x++)
 	{
 		if ((x % 3 == 0) || (x % 5 == 0))
 		{
 			y = x + y;
 		}
 	}
-	printf("%ld\n");
+	printf("%ld\n", y);
 
 	return (0);
 }
