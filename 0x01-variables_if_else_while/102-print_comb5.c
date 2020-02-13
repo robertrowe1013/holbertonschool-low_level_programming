2eb5e06162f1885e557e9d734a923329aaf164f6
robertrowe1013 <robertrowe1013@gmail.com>
2020-02-13 04:41:27 +0000

    task 13

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -6,55 +6,23 @@
   */
 int main(void)
 {
-	int ones1 = '0';
-	int tens1 = '0';
-	int ones2 = '0';
-	int tens2 = '0';
+	int first;
+	int second;
 
-	while (tens2 <= '8' && ones2 <= '9' && tens1 <= '9' && ones1 <= '9')
+	for (first = 0; first <= 98; first++)
 	{
-		putchar (tens2);
-		putchar (ones2);
-		putchar (' ');
-		putchar (tens1);
-		putchar (ones1);
-		if (tens2 == '8' && ones2 == '9' && tens1 == '9' && ones1 == '9')
+		for (second = first +1; second <= 99; second++)
 		{
-			;
-		}
-		else
-		{
-			putchar(',');
+			putchar((first / 10) + '0');
+			putchar((first % 10) + '0');
 			putchar(' ');
-		}
-		if (ones2 == '9')
-		{
-			ones2 = (tens2 + '1');
-			tens1 = (tens2 + '1');
-			ones1 = (tens1 + '1');
-		}
-		else
-		{
-			;
-		}
-		if (ones1 == '9' && tens1 == '9')
-		{
-			tens1 = (ones2 + '1');
-			ones1 = ones2;
-			ones2++;
-		}
-		else
-		{
-			;
-		}
-		if (ones1 == '9')
-		{
-			tens1++;
-			ones1 = '0';
-		}
-		else
-		{
-			ones1++;
+			putchar((second / 10) + '0');
+			putchar((second % 10) + '0');
+			if ((first + second) < 197)
+			{
+				putchar(',');
+				putchar(' ');
+			}
 		}
 	}
 	putchar('\n');
