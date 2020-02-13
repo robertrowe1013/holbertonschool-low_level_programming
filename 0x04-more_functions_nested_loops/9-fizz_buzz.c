add2ce1190d7659b10904866fe4c9c353ff76e65
robertrowe1013 <robertrowe1013@gmail.com>
2020-02-13 21:55:28 +0000

    task 9

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+/**
+  * main - print 1 - 100 3 is fizz, 5 is bang
+  *
+  * Return: 0
+  */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n <= 101; n++)
+	{
+		if (n == 101)
+		{
+			putchar('\n');
+		}
+		else if ((n % 5 == 0) && (n % 3 == 0))
+		{
+			printf("FizzBuzz ");
+		}
+		else if (n % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+		else if (n % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else
+		{
+			printf("%d ", n);
+		}
+	}
+	return (0);
+}
