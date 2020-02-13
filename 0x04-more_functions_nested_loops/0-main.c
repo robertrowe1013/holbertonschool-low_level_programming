5acceaf978e4ef1e2aea249d9e9200dd458b2196
robertrowe1013 <robertrowe1013@gmail.com>
2020-02-13 18:27:17 +0000

    corrected 0, 1, and 5

--- 0x04-more_functions_nested_loops/0-main.c
+++ 0x04-more_functions_nested_loops/0-main.c
@@ -10,9 +10,9 @@ int main(void)
 {
 	    char c;
 
-	        c = 'A';
+	        c = 100;
 		    printf("%c: %d\n", c, _isupper(c));
-		        c = 'a';
+		        c = 'Z';
 			    printf("%c: %d\n", c, _isupper(c));
 			        return (0);
 }
--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -7,19 +7,15 @@ void more_numbers(void)
 	int n;
 	int i;
 
-	for (i = 0; i <= 10; i++)
+	for (i = 0; i <= 9; i++)
 	{
 		for (n = 0; n <= 14; n++)
 		{
 			if (n > 9)
 			{
 				_putchar((n / 10) + '0');
-				_putchar((n % 10) + '0');
 			}
-			else
-			{
-				_putchar(n + '0');
-			}
+			_putchar((n % 10) + '0');
 		}
 		_putchar('\n');
 	}
