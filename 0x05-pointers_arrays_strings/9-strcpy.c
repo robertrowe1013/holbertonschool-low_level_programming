18801c08aca773582e09dedc90f5508f1b41983f
robertrowe1013 <robertrowe1013@gmail.com>
2020-02-18 17:00:20 +0000

    task 9

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -8,4 +8,17 @@
   */
 char *_strcpy(char *dest, char *src)
 {
+	int sl = 0;
+	int i;
 
+	while (src[sl] != '\0')
+	{
+		sl++;
+	}
+	for (i = 0; i < sl; i++)
+	{
+		*(dest + i) = *(src +i);
+	}
+
+	return (dest);
+}
