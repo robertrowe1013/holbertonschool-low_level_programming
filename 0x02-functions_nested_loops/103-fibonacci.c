93c68c6a4ab5e860eb48c7f3f505288070e6fe38
robertrowe1013 <robertrowe1013@gmail.com>
2020-02-18 22:52:53 +0000

    task 103

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -11,7 +11,19 @@ int main(void)
 	unsigned long a = 1;
 	unsigned long b = 2;
 	unsigned long c;
-	unsigned long n;
+	unsigned long n = 2;
 
-	for (i = 0, n < 4000000000; i++)
+	for (i = 0; n < 4000000000; i++)
 	{
+		c = a + b;
+		if (i % 2 == 0)
+		{
+			n = n + c;
+		}
+		a = b;
+		b = c;
+	}
+	printf("%lu\n", n);
+
+	return (0);
+}
