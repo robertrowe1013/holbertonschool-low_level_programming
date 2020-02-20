1a94cff1cac5e98298c359d9a09a58234e6f55fe
robertrowe1013 <robertrowe1013@gmail.com>
2020-02-20 19:39:55 +0000

    task 1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -9,3 +9,20 @@
   */
 char *_strncat(char *dest, char *src, int n)
 {
+	int sl = 0;
+	int i = 0;
+	
+	while (dest[sl] != '\0')
+	{
+		sl++;
+	}
+	while (i < n && src[i] != '\0')
+	{
+		dest[sl] = src[i];
+		i++;
+		sl++;
+	}
+	dest[sl] = '\0';
+	
+	return (dest);
+}
