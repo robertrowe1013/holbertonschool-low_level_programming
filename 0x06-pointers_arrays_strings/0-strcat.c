3224e7cafd3cfcbf8d4c791112ea0ed35e8584f2
robertrowe1013 <robertrowe1013@gmail.com>
2020-02-20 17:06:37 +0000

    task 0 swiched from while to for loops

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -11,14 +11,10 @@ char *_strcat(char *dest, char *src)
 	int sl = 0;
 	int i = 1;
 
-	while (dest[sl] != '\0')
-	{
-		sl ++;
-	}
-	while (src[-i] != '\0')
+	for (sl = 0; dest[sl] != '\0'; sl++)
+	for (i = 1; src[-i] != '\0'; i++)
 	{
 		dest[sl + i] = src[i - 1];
-		i++;
 	}
 	dest[sl + 1] = '\0';
 
