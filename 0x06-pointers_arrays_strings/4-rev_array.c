83b6d4fa81f2fe06f1af06271fb4c3c16ccaf214
robertrowe1013 <robertrowe1013@gmail.com>
2020-02-20 21:57:10 +0000

    task 4

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -3,8 +3,26 @@
   * reverse_array - reverse array
   * @a: array
   * @n: number
-  *
-  * Return: ?
   */
 void reverse_array(int *a, int n)
 {
+	int tmp;
+	int i;
+	int rev = n -1;
+
+	if (n % 2 == 0)
+	{
+		n = n / 2;
+	}
+	else 
+	{
+		n = (n - 1) / 2;
+	}
+	for (i = 0; i < n; i++)
+	{
+		tmp = a[i];
+		a[i] = a[rev];
+		a[rev] = tmp;
+		rev--;
+	}
+}
