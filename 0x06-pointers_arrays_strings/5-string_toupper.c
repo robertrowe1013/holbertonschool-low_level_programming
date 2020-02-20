6ae4568e6ce41eeefb99a083a207b0131a67eae6
robertrowe1013 <robertrowe1013@gmail.com>
2020-02-20 23:53:21 +0000

    task 5

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,6 +1,26 @@
 #include "holberton.h"
 /**
   * string_toupper - uppercase
+  * @s: string
+  *
+  * Return: string
   */
-char *string_toupper(char *)
+char *string_toupper(char *s)
 {
+	int i = 0;
+
+	while (s[i] != '\n')
+	{
+		if (s[i] >= 'a' && s[i] <= 'z')
+		{
+			s[i] -= 32;
+		}
+		else
+		{
+			;
+		}
+		i++;
+	}
+
+	return (s);
+}
