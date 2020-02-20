6b17009edfc11bd7fb71572addc32d90644d4e67
robertrowe1013 <robertrowe1013@gmail.com>
2020-02-20 21:15:44 +0000

    task 3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -3,8 +3,30 @@
   * _strcmp - compare string
   * @s1: string 1
   * @s2: string 2
-  * 
-  * Return: 0 is same, -1 if first unmatched char is less than second, 1 if greater
+  *
+  * Return: difference in s1 and s2
   */
-_strcmp(char *s1, char *s2)
+int _strcmp(char *s1, char *s2)
 {
+	int i = 0;
+
+	while (s1[i] - s2[i] == 0)
+	{
+		if (s1[i] == '\0' || s2[i] == '\0')
+		{
+			break;
+		}
+		else
+		{
+			i++;
+		}
+	}
+	if (s1[i] == '\0' && s2[i] == '\0')
+	{
+		return (0);
+	}
+	else
+	{
+		return (s1[i] - s2[i]);
+	}
+}
