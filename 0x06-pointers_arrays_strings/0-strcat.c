9a8bd18b137ea0922a3c8f38086606e83739dd60
robertrowe1013 <robertrowe1013@gmail.com>
2020-02-20 16:43:16 +0000

    task 0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -8,3 +8,19 @@
   */
 char *_strcat(char *dest, char *src)
 {
+	int sl = 0;
+	int i = 1;
+
+	while (dest[sl] != '\0')
+	{
+		sl ++;
+	}
+	while (src[-i] != '\0')
+	{
+		dest[sl + i] = src[i - 1];
+		i++;
+	}
+	dest[sl + 1] = '\0';
+
+	return (dest);
+}
---
+++ 0x06-pointers_arrays_strings/maintest.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+#include <stdio.h>
+
+int main(void)
+{
+	char s1[98] = "Hello ";
+	char s2[] = "world";
+	char *p;
+
+	p = _strcat(s1, s2);
+
+	return (0);
+}
