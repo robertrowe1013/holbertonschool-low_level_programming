5211c5b2a8fe8ce3815c29b20505f13effa5ca08
robertrowe1013 <robertrowe1013@gmail.com>
2020-02-21 02:28:53 +0000

    task 7 bad code

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -2,5 +2,29 @@
 /**
   * leet - l337sp34k
   */
-char *leet(char *)
+char *leet(char *s)
 {
+	int i = 0;
+	int a;
+	char arr[] = "AaEeOoTtLl43071";
+
+	while (s[i] != '\0')
+	{
+		a = 0;
+		while (a <= 10)
+		{
+			if (s[i] == (arr[a] || arr[a + 1]))
+			{
+				s[i] = arr[a + 11];
+				a += 2;
+			}
+			else
+			{
+				a += 2;
+			}
+		}
+		i++;
+	}
+
+	return (s);
+}
