8262b944818d261ee52d9172187ec4e93b9d3174
robertrowe1013 <robertrowe1013@gmail.com>
2020-02-21 03:09:59 +0000

    task 8

--- 0x06-pointers_arrays_strings/8-rot13.c
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -1,6 +1,31 @@
 #include "holberton.h"
 /**
   * rot13 - rot13 cypher
+  * @s: string
+  *
+  * Return: string
   */
-char *rot13(char *)
+char *rot13(char *s)
 {
+	int i = 0;
+	int a;
+	char arr[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char rot[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	while (s[i] != '\0')
+	{
+		a = 0;
+		while (a <= 52)
+		{
+			if (s[i] == arr[a])
+			{
+				s[i] = rot[a];
+				break;
+			}
+			a++;
+		}
+		i++;
+	}
+
+	return (s);
+}
