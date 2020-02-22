bc761526fbdac8ab006379e78cee7aaf65b4a9d4
robertrowe1013 <robertrowe1013@gmail.com>
2020-02-22 02:35:04 +0000

    task 100

--- 0x06-pointers_arrays_strings/100-print_number.c
+++ 0x06-pointers_arrays_strings/100-print_number.c
@@ -5,19 +5,30 @@
   */
 void print_number(int n)
 {
-	int digit = 0;
+	int digit = 1;
 	int num = n;
 
-	if (num < 0)
-		_putchar('-');
-	else if (num == 0)
+	if (num == 0)
 		_putchar('0');
 	else
 	{
 		while (num != 0)
 		{
-			num / 10;
-			digit++;
+			num /= 10;
+			digit *= 10;
 		}
-
+		num = n;
+		digit /= 10;
+		if (num < 0)
+		{
+			_putchar('-');
+			num *= -1;
+		}
+		while (digit != 1)
+		{
+			_putchar(((num / digit) % 10) + '0');
+			digit /= 10;
+		}
+		_putchar((num % 10) + '0');
+	}
 }
