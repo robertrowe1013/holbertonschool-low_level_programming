09c68bf6f3030571d8c04b2df4b207ca22cfdba3
robertrowe1013 <robertrowe1013@gmail.com>
2020-02-23 02:29:30 +0000

    accounted for leading 0 in c2

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -19,14 +19,17 @@ int main(void)
 	{
 		c1 = a1 + b1;
 		c2 = a2 + b2;
-		if (c2 > 100000000)
+		if (c2 > 1000000)
 		{
-			c2 %= 100000000;
+			c2 %= 1000000;
 			c1++;
 		}
 		if (c1 > 0)
 		{
-			printf(", %lu%lu", c1, c2);
+			if (c2 < 100000)
+				printf(", %lu0%lu", c1, c2);
+			else
+				printf(", %lu%lu", c1, c2);
 		}
 		else
 		{
