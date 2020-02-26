46a8fcdc3ede6ad07bdba924f60087a3e4f50f6a
robertrowe1013 <robertrowe1013@gmail.com>
2020-02-26 20:22:06 +0000

    task 3

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+  * factorial - factorial
+  * @n: number
+  */
+int factorial(int n)
+{
+	if (n < 0)
+		return (-1);
+	if (n == 0)
+		return (1);
+
+	return (n * (factorial(n-1)));
+}
