31761de0654835ef863fc046e1e9efe83f55800b
robertrowe1013 <robertrowe1013@gmail.com>
2020-02-27 03:47:04 +0000

    intial match check for 100

--- 0x08-recursion/100-wildcmp.c
+++ 0x08-recursion/100-wildcmp.c
@@ -8,7 +8,10 @@
   */
 int wildcmp(char *s1, char *s2)
 {
-	return (*s1);
+	if (*s1 == '\0' && *s2 == '\0')
+		return (1);
+	if (*s1 == *s2)
+		return (wildcmp(s1 + 1, s2 + 2));
 
-	return (*s2);
+	return (0);
 }
