0718aa2b67f2f84c35ef9ab95a318dcd7b2b62ae
robertrowe1013 <robertrowe1013@gmail.com>
2020-02-27 03:34:23 +0000

    task 100 frame

---
+++ 0x08-recursion/100-wildcmp.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+  * wildcmp - compare
+  * @s1: string 1
+  * @s2: string 2
+  *
+  * Return: 1 if match, 0 if not
+  */
+int wildcmp(char *s1, char *s2)
+{
+	return (*s1);
+
+	return (*s2);
+}
