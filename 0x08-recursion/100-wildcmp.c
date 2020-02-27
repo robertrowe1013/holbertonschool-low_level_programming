3672356162780f2904335dbb93071bcb9eb14dfd
robertrowe1013 <robertrowe1013@gmail.com>
2020-02-27 04:44:40 +0000

    added wildcard check

--- 0x08-recursion/100-wildcmp.c
+++ 0x08-recursion/100-wildcmp.c
@@ -1,4 +1,5 @@
 #include "holberton.h"
+char* wildcard(char *star);
 /**
   * wildcmp - compare
   * @s1: string 1
@@ -8,10 +9,23 @@
   */
 int wildcmp(char *s1, char *s2)
 {
+	if (*s2 == '*')
+		wildcard(s2);
 	if (*s1 == '\0' && *s2 == '\0')
 		return (1);
-	if (*s1 == *s2)
-		return (wildcmp(s1 + 1, s2 + 2));
+	else if (*s1 == '\0' || *s2 == '\0')
+		return (0);
+	else if (*s1 == *s2)
+		return (wildcmp(s1 + 1, s2 + 1));
 
 	return (0);
+}
+char* wildcard(char *s2)
+{
+	if (*s2 != '*')
+		return (s2);
+	else if (*s2 == '*')
+		return (wildcard(s2 + 1));
+
+	return (s2);
 }
