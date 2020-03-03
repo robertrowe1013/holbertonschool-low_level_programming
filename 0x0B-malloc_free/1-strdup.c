2b30e4a91289d55f581205dedace4a276155c578
robertrowe1013 <robertrowe1013@gmail.com>
2020-03-03 22:50:56 +0000

    betty formatting

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,16 +1,17 @@
 #include "holberton.h"
 #include <stdlib.h>
 /**
-    * _strdup - duplicate string
-      * @str: string
-        *
-	  * Return: \0 if string null, null if not enough mem, else pointer
-	    */
+ * _strdup - duplicate string
+ * @str: string
+ *
+ * Return: \0 if string null, null if not enough mem, else pointer
+ */
 char *_strdup(char *str)
 {
 	int sl = 0;
 	char *dup;
 	int i;
+
 	if (str == NULL)
 		return (NULL);
 	while (str[sl] != '\0')
@@ -18,11 +19,8 @@ char *_strdup(char *str)
 	dup = malloc(sl * (sizeof(char) + 1));
 	if (dup == NULL)
 		return (NULL);
-	dup_origin = dup;
 	for (i = 0; str[i] != 0; i++)
-	{
 		dup[i] = str[i];
-	}
 	dup[i] = '\0';
 
 	return (dup);
