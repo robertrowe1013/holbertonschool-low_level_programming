90f1d7cce448367c62e31102edc00f6171441482
robertrowe1013 <robertrowe1013@gmail.com>
2020-03-04 04:32:33 +0000

    task 100 frame

---
+++ 0x0B-malloc_free/100-strtow.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+#include <stdlib.h>
+/**
+  * strtow - string tow?
+  * @str: string
+  *
+  * Return: null if str is null, str is empty, or malloc fails, else string
+  */
+char **strtow(char *str)
+{
+	str = (void);
+
+	return (NULL);
+}
