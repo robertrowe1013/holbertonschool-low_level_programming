cb7dad526594f753fc1d53841b2c4d3e97afea03
robertrowe1013 <robertrowe1013@gmail.com>
2020-03-05 20:19:09 +0000

    task 2

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,32 @@
+#include "holberton.h"
+#include <stdlib.h>
+/**
+  * _calloc - allocate memory for arry
+  * @nmemb: array count
+  * @size: mem size
+  *
+  * Return: NULL if size is 0, nmemb is 0, or malloc fails. else pointer.
+  */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *ar;
+	unsigned int i = 0;
+
+	if (size < 1 || nmemb < 1)
+	{
+		return (NULL);
+	}
+	ar = malloc(nmemb * size);
+	if (ar == NULL)
+	{
+		return (NULL);
+	}
+	while (i < nmemb)
+	{
+		ar[i] = 0;
+		i++;
+	}
+	ar[i] = '\0';
+
+	return (ar);
+}
