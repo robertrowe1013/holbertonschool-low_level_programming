477bfc30d65aa49705f4566c793292b2487dc77d
robertrowe1013 <robertrowe1013@gmail.com>
2020-03-06 01:11:21 +0000

    task 100

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,40 @@
+#include "holberton.h"
+#include <stdlib.h>
+/**
+  * _realloc - rallocate memory
+  * @ptr: pointer
+  * @old_size: old size
+  * @new_size: new size
+  * 
+  * Return: Null if new size is 0 or malloc fails, else pointer
+  */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	char *np;
+	char *p;
+	unsigned int i, n;
+
+	if (ptr == NULL)
+	{
+		ptr = malloc(sizeof(char) * new_size);
+	}
+	if (new_size == old_size)
+		return (ptr);
+	if (new_size == 0)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	np = malloc(sizeof(char) * new_size);
+	if (new_size > old_size)
+		n = old_size;
+	else
+		n = new_size;
+	p = ptr;
+	for (i = 0; i <= n; i++)
+		np[i] = p[i];
+	free(ptr);
+	ptr = np;
+
+	return (ptr);
+}
