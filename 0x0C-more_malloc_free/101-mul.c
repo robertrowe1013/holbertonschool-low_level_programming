9033fca8b2915575fa7baeda624e6a7b9e020bd3
robertrowe1013 <robertrowe1013@gmail.com>
2020-03-06 01:42:09 +0000

    task 101 frame

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+  * main - nothing!!!
+  * @argc: argc
+  * @argv: argv
+  *
+  * Return: Everything!!!
+  */
+int main(int argc, char **argv)
+{
+	(void)argc;
+	(void)argv;
+
+	printf("Error\n");
+	exit(98);
+
+	return (0);
+}
