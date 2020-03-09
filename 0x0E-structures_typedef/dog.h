23b8083f46911d600b631b3151a7a9f1c2a59fbe
robertrowe1013 <robertrowe1013@gmail.com>
2020-03-09 22:36:11 +0000

    task 3

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -16,5 +16,11 @@ struct dog
 };
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
 
 #endif /* DOG_H */
