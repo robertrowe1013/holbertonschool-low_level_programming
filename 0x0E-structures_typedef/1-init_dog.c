98988573cdc33ff840fbc87ed39619dfe2ecd972
robertrowe1013 <robertrowe1013@gmail.com>
2020-03-09 19:58:17 +0000

    task 1 unfinshed

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,12 @@
+#include "dog.h"
+/**
+  * init_dog - adopt don't shop
+  * @d: doggo
+  * @name: Here boy!
+  * @age: New tricks?
+  * @owner: giver of treats and scritchens
+  */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	struct 
+}
