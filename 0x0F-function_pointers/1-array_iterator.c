8a047b71fef374450f760430d9e5b766072d12cb
robertrowe1013 <robertrowe1013@gmail.com>
2020-03-11 22:54:56 +0000

    betty syntax

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -11,8 +11,7 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 	unsigned int i;
 
 	if (array == NULL || action == NULL)
-		return;
+		exit(0);
 	for (i = 0; i < size; i++)
 		action(array[i]);
-	return;
 }
