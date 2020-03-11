f51965f022bbdbd6e698193df483ccf0a285ca38
robertrowe1013 <robertrowe1013@gmail.com>
2020-03-11 22:50:38 +0000

    task 1

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -8,7 +8,11 @@
   */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	(void)array;
-	(void)size;
-	(void)action;
+	unsigned int i;
+
+	if (array == NULL || action == NULL)
+		return;
+	for (i = 0; i < size; i++)
+		action(array[i]);
+	return;
 }
