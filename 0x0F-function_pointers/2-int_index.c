9c8c0e545b37c954c4a9d1bbf3e7b8e0c9055088
robertrowe1013 <robertrowe1013@gmail.com>
2020-03-12 01:03:33 +0000

    fixed betty syntax

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -13,14 +13,14 @@ int int_index(int *array, int size, int (*cmp)(int))
 	int i;
 
 	if (size <= 0)
-		return(-1);
+		return (-1);
 	if (array == NULL || cmp == NULL)
-		return(-1);
-	for(i = 0; i < size; i++)
+		return (-1);
+	for (i = 0; i < size; i++)
 	{
 		if (cmp(array[i]) != 0)
-			return(i);
+			return (i);
 	}
 
-	return(-1);
+	return (-1);
 }
