01c0828be782cc4810402fef8d952ee59f61f335
robertrowe1013 <robertrowe1013@gmail.com>
2020-03-14 21:14:13 +0000

    fixed compare sign

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -14,12 +14,12 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	va_start(list, n);
 	for (i = 0; i < n; i++)
 	{
-		arg = va_arg(list, char *);
+		arg = va_arg(list, char*);
 		if (arg == NULL)
 			printf("(nil)");
 		else
 			printf("%s", arg);
-		if ((i > n - 1) && (separator != NULL))
+		if ((i < n - 1) && (separator != NULL))
 			printf("%s", separator);
 	}
 	printf("\n");
