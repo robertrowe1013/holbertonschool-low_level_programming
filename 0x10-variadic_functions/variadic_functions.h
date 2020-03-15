b40f0eea3e78edd32b9639c5804dbe86700aa935
robertrowe1013 <robertrowe1013@gmail.com>
2020-03-15 18:28:49 +0000

    updated header

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -18,5 +18,9 @@ int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
+void p_char(va_list to_prt);
+void p_int(va_list to_prt);
+void p_fl(va_list to_prt);
+void p_str(va_list to_prt);
 
 #endif /* VERIADIC_FUCTIONS_H */
