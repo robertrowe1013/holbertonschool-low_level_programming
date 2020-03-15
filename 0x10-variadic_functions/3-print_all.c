e469bcff6af6ad57c1d49b11f4f4204f6bf5f353
robertrowe1013 <robertrowe1013@gmail.com>
2020-03-15 17:41:21 +0000

    task 3 compiling

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,6 +1,70 @@
-#include "variatic_functions.h"
+#include "variadic_functions.h"
 #include <stdio.h>
 /**
   * print_all - print all the things!
   * @format: string of types
   */
+void print_all(const char * const format, ...)
+{
+	fmt_t prt_fmt[] = {
+		{"c", p_char},
+		{"i", p_int},
+		{"f", p_fl},
+		{"s", p_str},
+		{NULL, NULL}
+	};
+	int i = 0;
+	int i2 = 0;
+	va_list list;
+
+	va_start(list, format);
+	while (format != NULL && format[i])
+	{
+		while (prt_fmt[i2].s != NULL)
+		{
+			if (*(prt_fmt[i2].s) == format[i])
+				prt_fmt[i2].prt(list);
+			i2++;
+		}
+		i++;
+	}
+	printf("\n");
+	va_end(list);
+}
+/**
+  * p_char - print char
+  * @to_prt: print
+  */
+void p_char(va_list to_prt)
+{
+	printf("%c", va_arg(to_prt, int));
+}
+/**
+  * p_int - print int
+  * @to_prt: print
+  */
+void p_int(va_list to_prt)
+{
+	printf("%d", va_arg(to_prt, int));
+}
+/**
+  * p_fl - print float
+  * @to_prt: print
+  */
+void p_fl(va_list to_prt)
+{
+	printf("%f", va_arg(to_prt, double));
+}
+/**
+  * p_str - print string
+  * @to_prt: print
+  */
+void p_str(va_list to_prt)
+{
+	char *s;
+
+	s = va_arg(to_prt, char *);
+	if (s == NULL)
+		s = "(nil)";
+	printf("%s", s);
+}
