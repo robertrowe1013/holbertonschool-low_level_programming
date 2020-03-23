56f00001da230265c8575787c4f322eea3c2cadf
robertrowe1013 <robertrowe1013@gmail.com>
2020-03-23 17:35:43 +0000

    task 0

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,20 +1,17 @@
 #include "lists.h"
 /**
-  * print_list - print list
+  * print_listint - print list
   * @h: head
   *
   * Return: number of nodes
   */
-size_t print_list(const list_t *h)
+size_t print_listint(const listint_t *h)
 {
 	int c = 0;
 
 	while (h != NULL)
 	{
-		if (h->str == NULL)
-			printf("[0] (nil)\n");
-		else
-			printf("[%d] %s\n", h->len, h->str);
+		printf("%d\n", h->n);
 		h = h->next;
 		c++;
 	}
