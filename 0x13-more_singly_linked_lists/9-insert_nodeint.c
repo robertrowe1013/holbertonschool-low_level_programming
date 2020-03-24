6dfefef2308c6b3967529392452aac17e8954871
robertrowe1013 <robertrowe1013@gmail.com>
2020-03-24 14:36:42 +0000

    added check if node is new head

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -20,6 +20,12 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		return (NULL);
 	newnode->n = n;
 	newnode->next = NULL;
+	if (idx = 0)
+	{
+		newnode->next = *head;
+		head = newnode;
+		return (newnode);
+	}
 	tmp = *head;
 	for (i = 0; i < (idx - 1); i++)
 	{
