76ac5a6da8098ea64885fb34d28357d353195bf5
robertrowe1013 <robertrowe1013@gmail.com>
2020-03-26 21:55:12 +0000

    task 1

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+/**
+  * print_binary - print binary
+  * @n: decimal number to convert
+  */
+void print_binary(unsigned long int n)
+{
+	int digit;
+	int tmp;
+	int printflag;
+
+	if (n == 0)
+		_putchar('0');
+	for (digit = 32; digit >= 0; digit--)
+	{
+		tmp = n >> digit;
+		if (tmp & 1)
+		{
+			_putchar('1');
+			printflag = 1;
+		}
+		else if (printflag == 1)
+			_putchar('0');
+	}
+}
