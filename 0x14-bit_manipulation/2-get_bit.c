7a18cad74966604387f6fc3c86d398f07d99f1be
robertrowe1013 <robertrowe1013@gmail.com>
2020-03-26 22:14:00 +0000

    task 2

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+  * get_bit - bit set at index
+  * @n: decimal number
+  * @index: bit index to check
+  *
+  * Return: -1 if error, else set of index
+  */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned int set;
+
+	set = (n >> index) & 1;
+	if (set == 0 || set == 1)
+		return (set);
+	
+	return (-1);
+}
