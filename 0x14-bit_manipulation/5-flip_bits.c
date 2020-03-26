bed337b39bfa05c876f7ea6c2fe2c1fe58e3721c
robertrowe1013 <robertrowe1013@gmail.com>
2020-03-26 23:33:41 +0000

    task 5

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+/**
+  * flip_bits - find number of bits that need flipping
+  * @n: first number
+  * @m: second number
+  *
+  * Return: number of bits to flip
+  */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned int flip;
+	unsigned int tmp;
+	int i;
+	unsigned int count = 0;
+
+	flip = n ^ m;
+	for (i = 31; i >= 0; --i)
+	{
+		tmp = flip >> i;
+		if (tmp & 1)
+			count++;
+	}
+
+	return (count);
+}
