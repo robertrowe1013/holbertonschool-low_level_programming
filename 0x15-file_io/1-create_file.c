fa8eb86a8f484b5cd50116f911b8b1c7c1ff82a7
robertrowe1013 <robertrowe1013@gmail.com>
2020-04-01 04:31:12 +0000

    task 1 complete

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -8,7 +8,7 @@
   */
 int create_file(const char *filename, char *text_content)
 {
-	int file;
+	int file, sl, writefile;
 
 	if (filename == NULL)
 		return (-1);
@@ -16,6 +16,16 @@ int create_file(const char *filename, char *text_content)
 	if (file == -1)
 		return (-1);
 	if (text_content != NULL)
-		return (1);
+	{
+		while (text_content[sl] != '\0')
+			sl++;
+	}
+	writefile = write(file, text_content, sl);
+	if (writefile == -1)
+	{
+		close(file);
+		return (-1);
+	}
+	close(file);
 	return (1);
 }
