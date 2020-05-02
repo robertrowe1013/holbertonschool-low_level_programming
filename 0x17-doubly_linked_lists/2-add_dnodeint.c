a128e36b211f9b9ee92b5e523043fc6359ab1b17
robertrowe1013 <robertrowe1013@gmail.com>
2020-05-02 21:39:32 +0000

    added prev pointers to correct nodes

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -10,12 +10,22 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 	dlistint_t *new_node;
 
-	new_node = *head;
-	*head = malloc(sizeof(dlistint_t));
-	if (*head == NULL)
+	new_node = malloc(sizeof(dlistint_t));
+	if (new_node == NULL)
 		return (NULL);
-	(*head)->n = n;
-	(*head)->next = new_node;
-
+	new_node->n = n;
+	new_node->prev = NULL;
+	if ((*head) == NULL)
+	{
+		(*head = new_node);
+		new_node->next = NULL;
+	}
+	else
+	{
+		new_node->next = (*head);
+		(*head)->prev = new_node;
+		(*head) = new_node;
+	}
+	
 	return (*head);
 }
