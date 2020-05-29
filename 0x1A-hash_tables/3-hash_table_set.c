5e100e6a78a5222d389a3a366c6a1303605ef892
robertrowe1013 <robertrowe1013@gmail.com>
2020-05-29 10:33:24 +0000

    empty string for key check

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -12,7 +12,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	unsigned int index;
 	hash_node_t *new_node;
 
-	if (key == NULL)
+	if (key == NULL || strcmp(key, "") == 0)
 		return (0);
 	new_node = malloc(sizeof(hash_node_t));
 	if (new_node == NULL)
