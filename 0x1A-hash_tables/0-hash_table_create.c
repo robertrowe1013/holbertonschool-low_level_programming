96564f5bbba494c341a7e438b6eb3762be3846df
robertrowe1013 <robertrowe1013@gmail.com>
2020-05-29 05:26:53 +0000

    task 0

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,23 @@
+#include "hash_tables.h"
+
+/**
+  * hash_table_create - create a hash table
+  * @size: size of table
+  *
+  * Return: pointer to hash table
+  */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *hashtable;
+
+	if (size < 1)
+		return (NULL);
+	hashtable = malloc(sizeof(hash_table_t));
+	if (hashtable == NULL)
+		return (NULL);
+	hashtable->array = (hash_node_t **)malloc(sizeof(hash_node_t) * size);
+	if (hashtable->array == NULL)
+		return (NULL);
+	hashtable->size = size;
+	return (hashtable);
+}
