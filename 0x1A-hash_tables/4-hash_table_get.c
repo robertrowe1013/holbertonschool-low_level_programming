11bb14719ee70530f1e6c9f22bb2e86bf7a0b458
robertrowe1013 <robertrowe1013@gmail.com>
2020-05-30 05:02:47 +0000

    traverse list at index for key

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -8,10 +8,18 @@
   */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
-	unsigned int index;
+	unsigned long int index;
+	hash_node_t *temp;
 
+	if (ht == NULL || key == NULL)
+		return (NULL);
 	index = hash_djb2((unsigned char *)key) % (ht->size);
-	if (strcmp(ht->array[index]->key, key) == 0)
-		return (ht->array[index]->value);
+	temp = ht->array[index];
+	while (temp != NULL)
+	{
+		if (strcmp(ht->array[index]->key, key) == 0)
+			return (ht->array[index]->value);
+		temp = temp->next;
+	}
 	return (NULL);
 }
