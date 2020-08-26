77e490eea9b7565cfff86bf58530108cc29ba277
robertrowe1013 <robertrowe1013@gmail.com>
2020-08-26 21:49:56 +0000

    fixed loop stop

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -14,7 +14,7 @@ int linear_search(int *array, size_t size, int value)
 
 	if (array == NULL)
 		return (-1);
-	for (i = 0; i <= size; i++)
+	for (i = 0; i < size; i++)
 	{
 		printf("Value checked at array[%d] = [%d]\n", i, array[i]);
 		if (array[i] == value)
