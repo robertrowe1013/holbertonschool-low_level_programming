2b0ddd079bc3fb75fcd9bd0723ce1b3ec2992a81
robertrowe1013 <robertrowe1013@gmail.com>
2020-08-27 09:11:21 +0000

    task 100

---
+++ 0x1E-search_algorithms/100-jump.c
@@ -0,0 +1,45 @@
+#include "search_algos.h"
+#include <math.h>
+
+/**
+  * jump_search - jump search
+  * @array: pointer to first element in search array
+  * @size: size of array
+  * @value: value to find
+  *
+  * Return: index where found or -1
+  */
+
+int jump_search(int *array, size_t size, int value)
+{
+	int  i, jump;
+	double root;
+
+	if (array == NULL)
+		return (-1);
+
+	root = sqrt((double)size);
+	jump = root;
+	i = 0;
+
+	while (array[i] < value)
+	{
+		printf("Value checked array[%d] = [%d]\n", i, i);
+		if (array[i + jump] >= value || i + jump >= (int)size)
+		{
+			printf("Value found between indexes [%d] and [%d]\n", i , i + jump);
+			break;
+		}
+		else
+			i = i + jump;
+	}
+	while(i < (int)size)
+	{
+		printf("Value checked array[%d] = [%d]\n", i, i);
+		if (array[i] == value)
+			return (i);
+		else
+			i++;
+	}
+	return (-1);
+}
